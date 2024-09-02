a0be9a168971d42477c1a56534706de38e5b3add
avdhutssh <avdhut.ssh@gmail.com>
2024-09-02 17:47:29 +0530

    POM -> ElementUtils -> added function SwitchTowindow

--- src/main/java/com/swag/labs/Utilities/ElementUtils.java
+++ src/main/java/com/swag/labs/Utilities/ElementUtils.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 public class ElementUtils extends BasePage {
 
     protected WebDriver driver;
+    protected Logger log;
     protected JavascriptExecutor js;
 
     public ElementUtils(WebDriver driver, Logger log) {
@@ -32,6 +33,25 @@ public class ElementUtils extends BasePage {
             js.executeScript("arguments[0].click()", ele);
         } catch (Exception e) {
             System.err.println("Exception occurred: " + e.getMessage());
+        }
+    }
+
+    public void SwitchTowindow(int windowNumber) {
+        waitForChildWindow(i);
+        String[] windowHandles = driver.getWindowHandles().stream().toArray(String[]::new);
+        try {
+            if (windowNumber > windowHandles.length) {
+                throw new RuntimeException(String.format(
+                        "The specified window number (%s) is greater than the " + "number of windows created (%s).",
+                        windowNumber, windowHandles.length));
+            }
+            driver.switchTo().window(windowHandles[windowNumber - 1]);
+            log.info("Successfully switched to window number: " + windowNumber);
+        } catch (Exception ex) {
+            throw new RuntimeException(
+                    String.format("Failed to switch to window number %s. The total number of open windows was %s.",
+                            windowNumber, windowHandles.length),
+                    ex);
         }
     }
 }
