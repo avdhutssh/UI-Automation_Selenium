87472719a1e72f7b152ac979ac4ad09e3f9fd32a
avdhutssh <avdhut.ssh@gmail.com>
2024-09-02 16:50:16 +0530

    POM -> ElementUtils -> added function clickOnElementJS

--- src/main/java/com/swag/labs/Utilities/ElementUtils.java
+++ src/main/java/com/swag/labs/Utilities/ElementUtils.java
@@ -1,15 +1,20 @@
 package com.swag.labs.Utilities;
 
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class ElementUtils extends BasePage {
 
     protected WebDriver driver;
+    protected JavascriptExecutor js;
 
-    public ElementUtils(WebDriver driver) {
-        super(driver);
+    public ElementUtils(WebDriver driver, Logger log) {
+        super(driver, log);
         this.driver = driver;
+        this.log = log;
+        this.js = (JavascriptExecutor) driver;
     }
 
     public void clickOnElement(WebElement element) {
@@ -19,6 +24,14 @@ public class ElementUtils extends BasePage {
         } catch (Exception e) {
             webElement.click();
             System.err.println("Failed to click on element " + e.getMessage());
+        }
+    }
+
+    public void clickOnElementJS(WebElement ele) {
+        try {
+            js.executeScript("arguments[0].click()", ele);
+        } catch (Exception e) {
+            System.err.println("Exception occurred: " + e.getMessage());
         }
     }
 }
