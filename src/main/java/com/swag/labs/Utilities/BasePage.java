31429735a5a436e4a9b5a386f33437782414632b
avdhutssh <avdhut.ssh@gmail.com>
2024-09-02 15:24:03 +0530

    POM -> BasePage -> added function waitForVisibilityOf

--- src/main/java/com/swag/labs/Utilities/BasePage.java
+++ src/main/java/com/swag/labs/Utilities/BasePage.java
@@ -1,9 +1,6 @@
 package com.swag.labs.Utilities;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -173,13 +170,27 @@ public class BasePage {
         return driver.getCurrentUrl();
     }
 
-    /**
-     * Wait for specific ExpectedCondition for the given amount of time in seconds
-     */
+
+    // Wait for specific ExpectedCondition for the given amount of time in seconds
+
     private void waitFor(ExpectedCondition<WebElement> condition, Integer timeOutInSeconds) {
         timeOutInSeconds = timeOutInSeconds != null ? timeOutInSeconds : 30;
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSeconds));
         wait.until(condition);
     }
-    
+
+
+    //     Wait for given number of seconds for element with given locator to be visible on the page
+    protected void waitForVisibilityOf(By locator, Integer... timeOutInSeconds) {
+        int attempts = 0;
+        while (attempts < 2) {
+            try {
+                waitFor(ExpectedConditions.visibilityOfElementLocated(locator),
+                        (timeOutInSeconds.length > 0 ? timeOutInSeconds[0] : null));
+                break;
+            } catch (StaleElementReferenceException e) {
+            }
+            attempts++;
+        }
+    }
 }
