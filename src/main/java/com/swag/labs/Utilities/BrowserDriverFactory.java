cc7f45f77831ee573435a2287d90a51d2af23a56
avdhutssh <avdhut.ssh@gmail.com>
2024-09-03 16:04:52 +0530

    POM -> BrowserDriverFactory -> added switch case for chrome and firefox with and without headless

---
+++ src/main/java/com/swag/labs/Utilities/BrowserDriverFactory.java
@@ -0,0 +1,61 @@
+package com.swag.labs.Utilities;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BrowserDriverFactory {
+    private ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private String browser;
+    private Logger log;
+
+    public BrowserDriverFactory(String browser, Logger log) {
+        this.browser = browser.toLowerCase();
+        this.log = log;
+    }
+
+    public WebDriver createDriver() {
+        log.info("Create driver: " + browser);
+
+        switch (browser) {
+            case "chrome":
+                driver.set(new ChromeDriver());
+                break;
+
+            case "firefox":
+                driver.set(new FirefoxDriver());
+                break;
+
+            case "chromeheadless":
+                log.info("Starting Chrome in Headless mode");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                driver.set(new ChromeDriver(chromeOptions));
+                break;
+
+            case "firefoxheadless":
+                log.info("Starting Firefox in Headless mode");
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                driver.set(new FirefoxDriver(firefoxOptions));
+                break;
+
+            default:
+                log.info("Do not know how to start: " + browser + ", starting chrome.");
+                driver.set(new ChromeDriver());
+                break;
+        }
+
+        driver.get().manage().window().maximize();
+        return driver.get();
+    }
+
+}
