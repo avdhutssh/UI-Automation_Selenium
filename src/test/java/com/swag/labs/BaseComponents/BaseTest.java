8898a2e78cb004ebd2e22838a4105091ff7fcc80
avdhutssh <avdhut.ssh@gmail.com>
2024-09-03 16:26:16 +0530

    POM -> BaseTest -> explicitely added browser property value from config.properties file using ConfigUtils

--- src/test/java/com/swag/labs/BaseComponents/BaseTest.java
+++ src/test/java/com/swag/labs/BaseComponents/BaseTest.java
@@ -1,16 +1,17 @@
 package com.swag.labs.BaseComponents;
 
 import com.swag.labs.Utilities.BrowserDriverFactory;
+import com.swag.labs.Utilities.ConfigurationUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.lang.reflect.Method;
+import java.util.Properties;
 
 public class BaseTest {
     protected WebDriver driver;
@@ -19,11 +20,14 @@ public class BaseTest {
     protected String testName;
     protected String testMethodName;
 
+    protected Properties prop = new ConfigurationUtils().getProperty();
+
+
     @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true)
-    public void setUp(Method method, @Optional("chrome") String browser, ITestContext ctx) {
+    public void setUp(Method method, ITestContext ctx) {
         log = LogManager.getLogger(testName);
-
+        String browser = prop.getProperty("browser", "chrome");
         BrowserDriverFactory factory = new BrowserDriverFactory(browser, log);
         driver = factory.createDriver();
         driver.manage().window().maximize();
