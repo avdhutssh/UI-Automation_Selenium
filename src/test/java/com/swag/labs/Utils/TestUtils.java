444eae2b5d690a27570831d4b212015d5ae217f8
avdhutssh <avdhut.ssh@gmail.com>
2024-09-03 09:56:59 +0530

    POM -> TestUtils -> added function takeScreenshot

--- src/test/java/com/swag/labs/Utils/TestUtils.java
+++ src/test/java/com/swag/labs/Utils/TestUtils.java
@@ -1,7 +1,12 @@
 package com.swag.labs.Utils;
 
 import com.swag.labs.BaseComponents.BaseTest;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -24,5 +29,16 @@ public class TestUtils extends BaseTest {
         }
     }
 
-    
+    protected void takeScreenshot(String fileName) {
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        String path = System.getProperty("user.dir") + File.separator + "test-output" + File.separator + "screenshots"
+                + File.separator + getTodaysDate() + File.separator + testSuiteName + File.separator + testName
+                + File.separator + testMethodName + File.separator + getSystemTime() + " " + fileName + ".png";
+        try {
+            FileUtils.copyFile(srcFile, new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
