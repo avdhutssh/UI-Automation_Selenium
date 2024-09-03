05355f1a790c88f78eaadb19dcc20b2d96dd5376
avdhutssh <avdhut.ssh@gmail.com>
2024-09-03 16:12:43 +0530

    POM -> BaseTest -> added method for setUp and tearDown

---
+++ src/test/java/com/swag/labs/BaseComponents/BaseTest.java
@@ -0,0 +1,40 @@
+package com.swag.labs.BaseComponents;
+
+import com.swag.labs.Utilities.BrowserDriverFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import java.lang.reflect.Method;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected Logger log;
+    protected String testSuiteName;
+    protected String testName;
+    protected String testMethodName;
+
+    @Parameters({"browser"})
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(Method method, @Optional("chrome") String browser, ITestContext ctx) {
+        log = LogManager.getLogger(testName);
+
+        BrowserDriverFactory factory = new BrowserDriverFactory(browser, log);
+        driver = factory.createDriver();
+        driver.manage().window().maximize();
+        this.testName = ctx.getCurrentXmlTest().getName();
+        this.testSuiteName = ctx.getSuite().getName();
+        this.testMethodName = method.getName();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        log.info("Close driver");
+        driver.quit();
+    }
+}
