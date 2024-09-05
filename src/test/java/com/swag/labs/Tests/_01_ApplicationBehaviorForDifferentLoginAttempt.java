ff2125c510416f0a68291594ac521713867b0946
avdhutssh <avdhut.ssh@gmail.com>
2024-09-05 18:01:32 +0530

    Decorator Pattern-> Added Data Provider and some require changes in _02_Verify_Successful_Login_Attempt_For_All_Usernames_Given_On_Login_Page

--- src/test/java/com/swag/labs/Tests/_01_ApplicationBehaviorForDifferentLoginAttempt.java
+++ src/test/java/com/swag/labs/Tests/_01_ApplicationBehaviorForDifferentLoginAttempt.java
@@ -1,14 +1,19 @@
 package com.swag.labs.Tests;
 
 import com.swag.labs.BaseComponents.BaseTest;
-import com.swag.labs.PageObjects.ProductsPage;
+import com.swag.labs.PageObjects.LoginPage;
 import com.swag.labs.Utilities.CsvDataProviders;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Map;
+import java.util.function.Consumer;
+
+import static com.swag.labs.BaseComponents.LoginDecorators.*;
 
 public class _01_ApplicationBehaviorForDifferentLoginAttempt extends BaseTest {
+
     @Test(dataProvider = "csvFileReader", dataProviderClass = CsvDataProviders.class)
     public void _01_Validate_Incorrect_Login_Attempt(Map<String, String> testData) {
         String username = testData.get("username");
@@ -19,15 +24,21 @@ public class _01_ApplicationBehaviorForDifferentLoginAttempt extends BaseTest {
         Assert.assertEquals(loginPage.getErrorMsg(), expectedMsg);
     }
 
-    @Test
-    public void _02_Verify_Successful_Login_Attempt_For_All_Usernames_Given_On_Login_Page() {
-        // TO-DO: Use Dataprovider and decorator pattern for this
-        String[] usernames = {"standard_user", "problem_user", "performance_glitch_user", "error_user",
-                "visual_user"};
-        for (String username : usernames) {
-            loginPage.loginWithUserInfo(username, "secret_sauce");
-            ProductsPage productsPage = new ProductsPage(driver, log);
-            productsPage.logout();
-        }
+    @Test(dataProvider = "getData")
+    public void _02_Verify_Successful_Login_Attempt_For_All_Usernames_Given_On_Login_Page(Consumer<LoginPage> consumer) {
+        consumer.accept(loginPage);
+    }
+
+    @DataProvider
+    public Object[] getData() {
+        return new Object[]{
+                validLogin1.andThen(successfulLogin),
+                validLogin2.andThen(successfulLogin),
+                validLogin3.andThen(successfulLogin),
+                validLogin4.andThen(successfulLogin),
+                validLogin5.andThen(successfulLogin),
+                invalidLogin1.andThen(unsuccessfulLogin),
+                invalidLogin2.andThen(unsuccessfulLogin)
+        };
     }
 }
