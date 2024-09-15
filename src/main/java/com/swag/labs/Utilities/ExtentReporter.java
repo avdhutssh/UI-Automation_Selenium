7836afb66a9d1a2d5956a592221754096312fb93
avdhutssh <avdhut.ssh@gmail.com>
2024-09-15 23:49:22 +0530

    ExtentReport -> Added ExtentReporter Utility

--- src/main/java/com/swag/labs/Utilities/ExtentReporter.java
+++ src/main/java/com/swag/labs/Utilities/ExtentReporter.java
@@ -1,5 +1,40 @@
 package com.swag.labs.Utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
 public class ExtentReporter {
 
+    public static ExtentReports generateExtentReport() {
+        String timestamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+//        File extentReportFile = new File(System.getProperty("user.dir") + "Reports//extentReport.html");
+        File reportsDir = new File(System.getProperty("user.dir") + File.separator + "Reports");
+        if (!reportsDir.exists()) {
+            reportsDir.mkdirs(); // Create the Reports directory if it doesn't exist
+        }
+        File extentReportFile = new File(reportsDir + File.separator + "extentReport.html");
+
+        ExtentReports extentReport = new ExtentReports();
+        ExtentSparkReporter sparkReporter = new ExtentSparkReporter(extentReportFile);
+        sparkReporter.config().setTheme(Theme.DARK);
+        sparkReporter.config().setReportName("Test Suite Execution for Sauce Lab");
+        sparkReporter.config().setDocumentTitle("Automation Execution Report");
+        sparkReporter.config().setTimeStampFormat("dd-MM-yyyy hh:mm:ss");
+        extentReport.attachReporter(sparkReporter);
+
+        Properties prop = new ConfigurationUtils().getProperty();
+
+        extentReport.setSystemInfo("Application URL", prop.getProperty("url"));
+        extentReport.setSystemInfo("Browser Name", prop.getProperty("browser"));
+        extentReport.setSystemInfo("Operating System", System.getProperty("os.name"));
+        extentReport.setSystemInfo("Java Version", System.getProperty("java.version"));
+        return extentReport;
+
+    }
 }
